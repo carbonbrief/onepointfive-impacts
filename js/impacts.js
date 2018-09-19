5a190f9426020615e68edabc051a71ba85dfb4ca
Rosamund Pearce <rosamundpearce@live.com>
2018-09-19 14:48:12 +0100

    Update intro droplinks

---
+++ js/impacts.js
@@ -0,0 +1,34 @@
+    // build DOM from data
+    
+    var impacts=[{
+        name: "background1",
+    },{
+        name: "background2"
+    },{
+        name: "SPECIES",
+        icon: "<img class='title-icon' src='../img/river.svg'>",
+        text: "Proportion of species losing >50% of their climatic range.",
+        info: "Extra info 1 - Lorem ipsum dolor sit amet",
+        background: "https://www.carbonbrief.org/wp-content/uploads/2018/04/Turtle-and-bleached-coral-on-Heron-Island-2.jpeg",
+        map: "australia",
+        location: "Australia"
+    },{
+        name: "RAINFALL",
+        icon: "<i class='fas fa-tint'></i>",
+        info: "Extra info 2 - Lorem ipsum dolor sit amet",
+        text: "Lorem ipsum dolor sit amet, eligendi scriptorem et nam.",
+        background: "https://www.carbonbrief.org/wp-content/uploads/2018/01/C5T9H7-rainfall.jpg",
+        map: "latin-america",
+        location: "Latin America"
+    },{
+        name: "GDP",
+        icon: "fas fa-dollar-sign",
+        info: "Extra info 3 - Lorem ipsum dolor sit amet",
+        text: "Global per capita GDP in 2100 (relative to no additional warming).",
+        background: "https://www.carbonbrief.org/wp-content/uploads/2015/07/Stock-wall-street-new-york-finance.jpg",
+        map: "global",
+        location: "Global"
+    },{
+        icon: "fas fa-info-circle",
+        text: "Extra info here.<br><br>Data compiled by <a href='https://www.carbonbrief.org/author/robertmcsweeney'target='_blank'>Robert McSweeney.</a> Interactive by <a href='https://www.carbonbrief.org/author/rospearce' >Rosamund Pearce.</a> Licensed under <a href='https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode'>Creative Commons.</a>",
+    }];
